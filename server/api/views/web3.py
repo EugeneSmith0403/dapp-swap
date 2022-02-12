701e0db2e683f77c4633e8f469b132e77aaa01d7
Eugene <evgentjkuznecov@MacBook-Air-Evgenij.local>
2022-02-12 18:58:43 +0700

    add logic for checking exist contract

--- server/api/views/web3.py
+++ server/api/views/web3.py
@@ -1,3 +1,4 @@
+from django.db import InternalError
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from django.conf import settings
@@ -32,35 +33,39 @@ class Web3Test(APIView):
         hash_contract = deploy_contract(**deployProperty)
         return Response({'result': 'Ok', 'hash_contract': hash_contract})
 
-    # contract_name,
-    # contract_class_name,
-    # props: { name, symbol, _amount, partialPrice }
-    # owner_wallet_address
+    # Create smart contract
+    # @param contract_name,
+    # @param contract_class_name,
+    # @param props: { name, symbol, _amount, partialPrice }
+    # @param owner_wallet_address
+    # @return
     def post(self, request):
-        try:
-            contract_name = request.data['contract_name']
-            contract_class_name = request.data['contract_class_name']
-            props = request.data['props']
-            owner_wallet_address = request.data['owner_wallet_address']
-
-            deployProperty = {
-                'contract_name': contract_name,
-                'contract_class_name': contract_class_name,
-                'contract_props': props,
-                'provider': provider,
-                'owner_wallet_address': owner_wallet_address
-            }
-            address_contract, abi = deploy_contract(**deployProperty)
+        contract_name = request.data['contract_name']
+        item = DeployedContract.objects.all().filter(contract_name=contract_name)
+        if not item:
             try:
-                DeployedContract(
-                    contract_name=contract_name,
-                    address_contract=address_contract,
-                    abi=abi,
-                    address_wallet=owner_wallet_address,
-                ).save()
-            except Exception:
-                raise Exception('Something wen\'t wrong with saving model to database!')
+                contract_class_name = request.data['contract_class_name']
+                props = request.data['props']
+                owner_wallet_address = request.data['owner_wallet_address']
 
-            return Response({'result': 'Ok', 'hash_contract': address_contract})
-        except Exception:
-            raise Exception('Check you arguments!')
+                deployProperty = {
+                    'contract_name': contract_name,
+                    'contract_class_name': contract_class_name,
+                    'contract_props': props,
+                    'provider': provider,
+                    'owner_wallet_address': owner_wallet_address
+                }
+                address_contract, abi = deploy_contract(**deployProperty)
+                try:
+                    DeployedContract(
+                        contract_name=contract_name,
+                        address_contract=address_contract,
+                        abi=abi,
+                        address_wallet=owner_wallet_address,
+                    ).save()
+                except InternalError:
+                    raise InternalError('Something wen\'t wrong with saving model to database!')
+                return Response({'status': 'ok', 'result': {'hash_contract': address_contract}})
+            except InternalError:
+                raise InternalError('Check you arguments!')
+        return {'result': 'contract Existed', 'status': 'ok'}
